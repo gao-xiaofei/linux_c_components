34a6b6258eae9c7dcef859b18d234b14f7582fb4
gao <gao_saifei@163.com>
2026-01-17 23:09:47 +0800

    feature: 完善断联后重连功能

--- components/doso_mqtt/test/main_mqtt.c
+++ components/doso_mqtt/test/main_mqtt.c
@@ -10,15 +10,45 @@
 // gcc doso_mqtt.c -o demo_test  -lpthread -lpaho-mqtt3c
 
 
-#define ADDRESS     "tcp://broker.emqx.io:1883"
-#define USERNAME    "emqx"
-#define PASSWORD    "public"
-#define CLIENTID    "c-client"
-#define QOS         0
-#define TOPIC       "topic/c-test"
-#define TIMEOUT     10000L
+#define ADDRESS             "tcp://broker.emqx.io:1883"
+#define USERNAME            "emqx"
+#define PASSWORD            "public"
+#define CLIENTID            "c-client"
+#define QOS                 0
+#define TOPIC               "topic/c-test"
+#define TIMEOUT             10000L
+#define RETRY_DELAY_MS      2000
 
-static int8_t is_alive = 0;
+#define SUB_TOPIC_1 "topic/c-test"
+#define SUB_TOPIC_2 "topic/c-test2"
+
+static int8_t is_alive = false;
+int retry_count = 0;
+
+int doso_sync_mqtt_subscribe(MQTTClient client)
+{
+    int rc = MQTTClient_subscribe(client, SUB_TOPIC_1, QOS);
+    if(rc != MQTTCLIENT_SUCCESS){
+        printf("MQTTClient_subscribe failed: %d\n", rc);
+    }
+    rc = MQTTClient_subscribe(client, SUB_TOPIC_2, QOS);
+    if(rc != MQTTCLIENT_SUCCESS){
+        printf("MQTTClient_subscribe failed: %d\n", rc);
+    }
+    return rc;
+}
+
+int doso_sync_mqtt_unsubscribe(MQTTClient client)
+{
+    int rc = MQTTClient_unsubscribe(client, SUB_TOPIC_1);
+    if(rc != MQTTCLIENT_SUCCESS){
+        printf("MQTTClient_unsubscribe failed: %d\n", rc);
+    }
+    rc = MQTTClient_unsubscribe(client, SUB_TOPIC_2);
+    if(rc != MQTTCLIENT_SUCCESS){
+        printf("MQTTClient_unsubscribe failed: %d\n", rc);
+    }
+}
 
 int doso_sync_mqtt_publish(MQTTClient client, char *topic, char *payload, uint16_t payload_len) 
 {
@@ -46,31 +76,10 @@ void connect_lost(void* context, char* cause) {
     MQTTClient client = *(MQTTClient*)context;
     
     printf("=== 连接丢失！原因：%s ===\n", (cause != NULL) ? cause : "未指定");
+    doso_sync_mqtt_unsubscribe(client);
+    MQTTClient_disconnect(client, TIMEOUT);
+    MQTTClient_destroy(&client);
     is_alive = false;
-
-    int retry_count = 0;          // 重试计数器
-    const int MAX_RETRY = 5000;      // 最大重试次数
-    const int RETRY_INTERVAL = 2; // 重试间隔（秒）
-    
-    while (retry_count < MAX_RETRY) {
-        printf("尝试重连mqtt服务器（第%d/%d次）...\n", retry_count + 1, MAX_RETRY);
-        
-        // 调用MQTTClient_connect尝试重连（参数需与初始连接一致）
-        MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-        conn_opts.username = USERNAME;
-        conn_opts.password = PASSWORD;
-        
-        int connect_rc = MQTTClient_connect(client, &conn_opts);
-        if (connect_rc == MQTTCLIENT_SUCCESS) {
-            printf("重连mqtt服务器成功！\n");
-            is_alive = true;
-            return;  // 重连成功，退出回调函数
-        } else {
-            printf("重连mqtt服务器失败，错误码：%d\n", connect_rc);
-            retry_count++;
-            sleep(RETRY_INTERVAL);  // 间隔指定时间后重试
-        }
-    }
     
 }
 
@@ -95,26 +104,6 @@ void *Thread1_Loop()
     int i = 0;
     MQTTClient client;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID, 0, NULL);
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    conn_opts.username = USERNAME;
-    conn_opts.password = PASSWORD;
-
-    MQTTClient_setCallbacks(client, &client, connect_lost, on_message, NULL);
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
-    } else {
-        printf("Connected to MQTT Broker!\n");
-        is_alive = true;
-    }
-
-    // TODO: 重连后重新subscribe对应topic
-    rc = MQTTClient_subscribe(client, TOPIC, QOS);
-    if(rc != MQTTCLIENT_SUCCESS){
-        printf("MQTTClient_subscribe failed: %d\n", rc);
-    }
-
     for (;;) {
      
         if(is_alive){
@@ -122,7 +111,28 @@ void *Thread1_Loop()
             snprintf(payload, 32, "message-%d", i++);
             doso_sync_mqtt_publish(client, TOPIC, payload, 32);
         }else{
-            printf("MQTT client is not alive! \n");
+            printf("尝试连接mqtt服务器（第%d次）...\n", retry_count + 1);
+
+            MQTTClient_create(&client, ADDRESS, CLIENTID, 0, NULL);
+            MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+            conn_opts.username = USERNAME;
+            conn_opts.password = PASSWORD;
+            MQTTClient_setCallbacks(client, &client, connect_lost, on_message, NULL);
+            int connect_rc = MQTTClient_connect(client, &conn_opts);
+            if (connect_rc == MQTTCLIENT_SUCCESS) {
+                printf("连接mqtt服务器成功！\n");
+                doso_sync_mqtt_subscribe(client);
+                is_alive = true;
+                return;  // 重连成功，退出回调函数
+            } else {
+                printf("连接mqtt服务器失败，错误码：%d\n", connect_rc);
+                
+                MQTTClient_disconnect(client, TIMEOUT);
+                MQTTClient_destroy(&client);
+                is_alive = false;
+                retry_count++;
+                usleep(RETRY_DELAY_MS*1000);  
+            }
         }
 
         usleep(1*100*1000);
